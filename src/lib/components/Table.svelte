02701698e8b60330c60950fa6f50e9bc233e8cf3
mystica2000 <mysticainf@gmail.com>
2024-02-27 22:23:41 +0530

    feat: sort

--- src/lib/components/Table.svelte
+++ src/lib/components/Table.svelte
@@ -5,6 +5,7 @@
   import { dataStoreInstance } from "../../store/dataStore";
   import Menu from "./Menu.svelte";
   import { writable } from "svelte/store";
+  import SortIcon from "../../icon/sortIcon.svelte";
 
   export let itemsPerPage = 10;
 
@@ -22,9 +23,6 @@
       dataStoreInstance.dispatch({ type: "bulkInsert", obj: result });
 
       totalPages = Math.ceil(result.length / itemsPerPage);
-      console.log(totalPages);
-      console.log(result);
-      console.log(itemsPerPage);
     } catch (err) {
       console.log("Something went wrong - ", err);
     }
@@ -47,18 +45,50 @@
       currentPage.update((aCurrentPage) => aCurrentPage - 1);
     }
   };
+
+  const applySortByCount = () => {
+    result.sort((a, b) => a.id - b.id);
+    currentPage.set(1);
+  };
+
+  const applySortByDate = () => {
+    result.sort(compareDates);
+    currentPage.set(1);
+  };
+
+  // Function to compare dates for sorting
+  const compareDates = (dateString1, dateString2) => {
+    const date1 = new Date(dateString1.date);
+    const date2 = new Date(dateString2.date);
+
+    // Compare the dates
+    if (date1 < date2) {
+      return -1;
+    } else if (date1 > date2) {
+      return 1;
+    } else {
+      return 0;
+    }
+  };
 </script>
 
 <table>
   <thead>
-    <th>Date</th>
+    <th
+      ><span class="sort"
+        >Date<button on:click={applySortByDate}><SortIcon /></button></span
+      ></th
+    >
     <th>Minutes</th>
-    <th>Count</th>
+    <th
+      ><span class="sort"
+        >Count <button on:click={applySortByCount}><SortIcon /></button></span
+      ></th
+    >
     <th></th>
   </thead>
   <tbody>
     {#if result}
-      <!-- {#each result as { count, time, date }, i} -->
       {#each getCurrentPageData($currentPage) as row (row.id)}
         <tr>
           <td>{row.date}</td>
@@ -111,6 +141,12 @@
   td {
     border: 1px solid black;
     border-radius: 10px;
+  }
+
+  .sort {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
   }
 
   .menu {
